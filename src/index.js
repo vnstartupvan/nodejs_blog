da0c924e4ad6404ef0e44c189728c2edb89d6655
vnstartupvan <vn.startup99@gmail.com>
2022-09-26 21:08:06 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const app = express();
 const port = 3001;
 app.use(express.static(path.join(__dirname, 'public')));
 //HTTPS Logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 //Template engine
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('views', path.join(__dirname, 'resouce/views'));
@@ -17,6 +19,13 @@ app.get('/', (req, res) => {
 });
 app.get('/news', (req, res) => {
     res.render('news');
+});
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+app.post('/search', (req, res) => {
+    console.log(req.body, 123)
+    res.send("")
 });
 
 
